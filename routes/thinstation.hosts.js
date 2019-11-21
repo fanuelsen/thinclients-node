054c9a7b3838062b1d62537a90af596b861e7bf9
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2019-11-21 18:37:20 +0100

    test4

--- routes/thinstation.hosts.js
+++ routes/thinstation.hosts.js
@@ -5,13 +5,15 @@ var ThinClientModel = require('../models/thinclient');
 router.get("/", async (request, response) => {
     try {
         var result = await ThinClientModel.find().exec();
+        var output = "# THINSTATION HOSTS";
+        result.forEach(element => {
+            output += "\n" + element.name + "\t" + element.mac + "\t" + element.settings;
+        });
         response.setHeader('Content-type', 'application/octet-stream');
         response.setHeader('Accept-Ranges', 'bytes');
-        response.setHeader('Cache-Control', 'public, max-age=0')
-        response.write("# THINSTATION HOSTS");
-        result.forEach(element => {
-            response.write("\n" + element.name + "\t" + element.mac + "\t" + element.settings);
-        });
+        response.setHeader('Content-Length', output.length);
+        response.setHeader('Cache-Control', 'public, max-age=0');
+        response.write(output);
         response.end();
     } catch (error) {
         response.status(500).send(error);
