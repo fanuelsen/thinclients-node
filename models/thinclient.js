7fbfca9de7b7d27731ea7945b32c5a71eb3118e3
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2019-11-27 15:02:49 +0100

    changed defaults

--- models/thinclient.js
+++ models/thinclient.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var ThinClientSchema = new Schema({
     name: { type: String, required: true },
     mac: { type: String, required: true },
-    settings: { type: String, default: "1920-1080-SS" }
+    settings: { type: String, default: "1440" }
 });
 
 module.exports = mongoose.model('thinclient', ThinClientSchema)
